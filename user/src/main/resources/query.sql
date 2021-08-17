ce36c4e09f82e0b2d12a26ddde3b90b8f236a55d
Shivendraa <kalseshivendra@gmail.com>
2021-08-17 21:12:42 +0530

    05 - Api Gateway for Loan, Account, Employee and User Microservices

---
+++ user/src/main/resources/query.sql
@@ -0,0 +1,3 @@
+create table role (ro_id int, ro_name varchar(20));
+create table user (us_id int, us_username varchar(20), us_password varchar(20));
+create table user_role(ur_ro_id int, ur_us_id int);
