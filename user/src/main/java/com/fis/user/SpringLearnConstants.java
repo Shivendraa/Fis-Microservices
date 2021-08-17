2509328770d7bc8eae8f79b03e1dea1d11b5d7ca
Shivendraa <kalseshivendra@gmail.com>
2021-08-17 11:12:22 +0530

    03 - Monolith to Microservice Employee and User

---
+++ user/src/main/java/com/fis/user/SpringLearnConstants.java
@@ -0,0 +1,9 @@
+package com.fis.user;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SpringLearnConstants {
+	public static final Logger LOGGER = LoggerFactory.getLogger(UserApplication.class);
+
+}
